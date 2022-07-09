56813854be02bb9d54a0f471577339f1173b6b99
Vyacheslav Nagornov <nodegopher@gmail.com>
2022-07-09 16:40:34 +0000

    Update deploy/deploy.sh, readme, main.go

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
 
-kill -9 $(pgrep webserver)
 cd ~/www/devops
 git pull git@gitlab.com:gEdisonLeun/devops.git
 ./webserver &
+
+# sudo chmod 666 /var/run/docker.sock  -- if dont login
+docker login -u morheus -p morheus1917
+docker pull morheus/go_app
+docker container rm -f go_app_latest || true
+docker run -d -p 8080:8080 --name go_app_latest morheus/go_app
