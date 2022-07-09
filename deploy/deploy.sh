84d3668f88d48754f83ad5672d3aafbb16409e08
Vyacheslav <nodegopher@gmail.com>
2022-07-09 23:08:42 +0300

    fix

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 # sudo chmod 666 /var/run/docker.sock  -- if don't login
-docker login -u morheus -p morheus1917
+# docker login -u @WRITE_HERE_YOUR_GITLAB_USER@ -p @WRITE_HERE_YOUR_GITLAB_USER@ #### REWRITE !!
 docker pull morheus/go_app
 docker container rm -f go_app_latest || true
 docker run -d -p 8080:8080 --name go_app_latest morheus/go_app
